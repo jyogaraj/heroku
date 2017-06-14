ab8c9685f9a1de725487b21e7ed51a29d47dcf0c
jyogaraj <yogi.instruzz@gmail.com>
2017-06-14 15:12:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
   var path = require('path');
   //var favicon = require('serve-favicon');
   //var logger = require('morgan');
-  var cookieParser = require('cookie-parser');
-  var bodyParser = require('body-parser');
+
   const sass = require('node-sass-middleware');
 
   var index = require('./routes/index');
@@ -18,9 +17,7 @@
   // uncomment after placing your favicon in /public
   //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
   //app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cookieParser());
+
   app.use(express.static(path.join(__dirname, 'public')));
 
   app.use(sass({
