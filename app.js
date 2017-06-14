23647b96f5780643bdfc346836a37bc85de420c1
jyogaraj <yogi.instruzz@gmail.com>
2017-06-14 14:57:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-var express = require('express');
-var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-const sass = require('node-sass-middleware');
+  var express = require('express');
+  var path = require('path');
+  //var favicon = require('serve-favicon');
+  //var logger = require('morgan');
+  var cookieParser = require('cookie-parser');
+  var bodyParser = require('body-parser');
+  const sass = require('node-sass-middleware');
 
-var index = require('./routes/index');
+  var index = require('./routes/index');
 
-var app = express();
+  var app = express();
 
-// view engine setup
-//app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+  // view engine setup
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  //app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(sass({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public')
-}));
+  app.use(sass({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public')
+  }));
 
-app.use('/', index);
-app.use('/home', index);
+  app.use('/', index);
+  app.use('/home', index);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
-app.listen('8080'), () => {
-  //console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('.'), app.get('port'), app.get('env'));
-  console.log('  Press CTRL-C to stop\n');
-});
 
-module.exports = app;
+  app.listen(app.get('port'), function () {
+      console.log("Express server listening on port " + app.get('port'));
+  });
+
+  module.exports = app;
 
